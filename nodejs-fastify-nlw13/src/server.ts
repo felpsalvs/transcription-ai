2b93307b6da825d0a8cf562d2a6223dcf056bf5f
Felipe Alves <felipe.alves@svm.com>
2023-09-28 15:51:41 -0300

    update package.json, pnpm-lock.yaml, generate-ai-completion.ts and server.ts

--- nodejs-fastify-nlw13/src/server.ts
+++ nodejs-fastify-nlw13/src/server.ts
@@ -2,7 +2,7 @@ import { fastify } from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
-import { generateAICompletionRoute } from "./routes/generate-ai-completion";
+import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
 import { fastifyCors } from "@fastify/cors";
 
 const app = fastify();
@@ -14,7 +14,7 @@ app.register(fastifyCors, {
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
-app.register(generateAICompletionRoute);
+app.register(generateAiCompletionRoute);
 
 app
   .listen({
